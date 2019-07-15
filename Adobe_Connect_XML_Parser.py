760211ba11991b8f5bc0e3602381628f2cad9a3f
jehalley <43151851+jehalley@users.noreply.github.com>
2019-07-15 16:10:28 -0700

    Update Adobe_Connect_XML_Parser.py

--- Adobe_Connect_XML_Parser.py
+++ Adobe_Connect_XML_Parser.py
@@ -13,13 +13,18 @@ import pytz
 import re
 import time
 
-def get_index_stream_xml_path(recording_folder_path):
-    indexstream = "indexstream.xml"
-    return recording_folder_path+indexstream
-
-def get_student_ids_and_pIDs(index_stream_xml_path):
+def get_index_stream(recording_folder_path):
+    '''
+    indexstram.xml is a file in the adobe connect recordings folder that
+    indicates student names and IDs when students enter the session, come on 
+    camera, come on microphone, and when they use the status buttons
+    '''
+    index_stream_xml_path = recording_folder_path + "indexstream.xml"
     with open(index_stream_xml_path) as filepath:
         index_stream = BeautifulSoup(filepath,"xml")
+    return index_stream
+
+def get_student_ids_and_pIDs(index_stream):
     #this will return a result set that can be used as an index to find 
     #the record that contains id and pID for each student
     student_names_index = index_stream.find_all('fullName')
@@ -90,16 +95,28 @@ def get_results_by_name_from_results_by_id(dict_of_results_by_id,student_ids):
     return results_with_names_subbed_for_ids
    
 
-def get_ftstage_file_path(recording_folder_path):
+def get_ftstage(recording_folder_path):
+    '''ftstage is the xml file describing what happens with the camera streams
+        in the video pod, much of this information is in the indexstream xml
+        file as well, but not the camera pause info. ftstage is used to find 
+        times when students pause the camera. ftstage files always start with
+        "ftstage" and end with ".xml" but they have different numbers in 
+        different recordings so a wildcard is used to find the file in the 
+        recording folder
+        '''
     ftstage_wildcard = "ftstage*.xml"
-    ftstage_file_path = glob.glob(recording_folder_path+ftstage_wildcard)
-    return ftstage_file_path[0]
+    '''
+    glob searches for the wildcard and returns a list of results. index is to
+    get the first result from the resulting list
+    ''' 
+    ftstage_file_path = glob.glob(recording_folder_path + ftstage_wildcard)[0]
+    with open(ftstage_file_path) as filepath:
+        ftstage = BeautifulSoup(filepath,"xml")
+    return ftstage
 
     
-def get_camera_contributions(index_stream_xml_path,ftstage_file_path,student_ids):
-    with open(index_stream_xml_path) as filepath:
-        index_stream = BeautifulSoup(filepath,"xml")
-    #get time when student came on camera
+def get_camera_contributions(index_stream,ftstage,student_ids):
+    #get time when student came on camera from index stream soup
     camera_starts_index = index_stream.find_all(string = 'streamAdded')
     camera_start_ids = []
     camera_start_times = []
@@ -153,8 +170,7 @@ def get_camera_contributions(index_stream_xml_path,ftstage_file_path,student_ids
     having students with ids but with no camera time interfere with later calcs
     so I will give all of the students that did not appear on camera a start
     and end time that is is the same as the end of class
-    '''
-     
+    ''' 
     for k in student_camera_stop_times.keys():
         if len(student_camera_start_times[k]) == 0:
             student_camera_stop_times[k] = [0]
@@ -168,9 +184,7 @@ def get_camera_contributions(index_stream_xml_path,ftstage_file_path,student_ids
         student_minutes_with_camera_on[k] += total_time
         
         
-    #get total time student pauses camera
-    with open(ftstage_file_path) as filepath:
-        ftstage = BeautifulSoup(filepath,"xml")
+    #get total time student pauses camera from ftstage soup
     pause_change_index = ftstage.find_all(string ="updateVideoPauseStatus")
     pause_start_ids = []
     pause_start_times = []
@@ -255,10 +269,22 @@ def get_camera_contributions(index_stream_xml_path,ftstage_file_path,student_ids
         if len(combined_pause_stop_times[k])>len(student_pause_start_times[k]):
             del(combined_pause_stop_times[k][len(combined_pause_stop_times[k])-1])
     
+    #many of the stop times will not correspond to real mic starts, so they should be removed
+    clean_stops = []
+    clean_stops_ids = []
+    for k in student_pause_start_times.keys():
+        for start_time in range(len(student_pause_start_times[k])):
+            clean_stops_ids.append(k)
+            clean_stops.append(next((x for x in combined_pause_stop_times[k] if x >student_pause_start_times[k][start_time]),student_pause_start_times[k][start_time] ))
+    
+    student_clean_stop_times = defaultdict(list)
+    for student_id, stop_time in zip(clean_stops_ids,clean_stops):
+        student_clean_stop_times[student_id].append(stop_time)
+    
     #determine the total time the student had the camera paused
     student_minutes_with_camera_paused = defaultdict(int)
     for k in combined_pause_stop_times.keys():
-        times = [a-b for a,b in zip(combined_pause_stop_times[k],student_pause_start_times[k])]
+        times = [a-b for a,b in zip(student_clean_stop_times[k],student_pause_start_times[k])]
         total_time = sum(times)/1000/60
         student_minutes_with_camera_paused[k] += total_time
     
@@ -289,9 +315,7 @@ def get_camera_contributions(index_stream_xml_path,ftstage_file_path,student_ids
     
             )
      
-def get_microphone_contributions(index_stream_xml_path,student_ids):
-    with open(index_stream_xml_path) as filepath:
-        index_stream = BeautifulSoup(filepath,"xml")
+def get_microphone_contributions(index_stream,student_ids):
     #get times when student has a microphone change (turns it on OR off)
     mic_change_index = index_stream.find_all(string = 'userVoipStatusChanged')
     mic_start_ids = []
@@ -373,24 +397,24 @@ def get_microphone_contributions(index_stream_xml_path,student_ids):
     get_results_by_name_from_results_by_id(student_fraction_of_instructor_mic,student_ids)
             )
 
-def get_chat_contributions(index_stream_xml_path,student_pIDs):
+def get_chat_contributions(index_stream,student_pIDs):
     '''
     ftchatX logs record time of chat message as unixtime code multiplied by 1000
     in PST. The start date in indexstream is a readable string stating the 
     in Greenwich mean time zone. This code will strip the time from 
     indexstream.xml and convert it into the same format used in the ftchat logs
     ''' 
-    #get indexstream timestamp
-    with open(index_stream_xml_path) as filepath:
-        index_stream = BeautifulSoup(filepath,"xml")
-        start_date = index_stream.root.Message.Array.String.next_sibling.next_sibling.next_sibling.next_sibling.text
+    #get get start date from indextream timestamp
+    start_date = index_stream.root.Message.Array.String.next_sibling.next_sibling.next_sibling.next_sibling.text
     
     #convert to format used in ftchats
     start_timestamp = datetime.fromtimestamp(time.mktime(time.strptime(start_date)))
+    
     #remove time information to capture chats after class starts but before recording
     start_day_timestamp = datetime(start_timestamp.year, start_timestamp.month, start_timestamp.day)
     old_timezone = pytz.timezone("Greenwich")
     new_timezone = pytz.timezone("US/Pacific")
+    
     #all of the timestamps used by AC are multiplied by 1000
     corrected_start_timestamp = 1000*(datetime.timestamp(old_timezone.localize(start_day_timestamp).astimezone(new_timezone)))
     
@@ -398,12 +422,11 @@ def get_chat_contributions(index_stream_xml_path,student_pIDs):
     ftchat_wildcard = "ftchat*.xml"
     ftchat_file_path_list = glob.glob(recording_folder_path+ftchat_wildcard)
     
-    #loop through the list finding all messages
+    #loop through the list finding all messages chat_times, and chat_lengths
     chat_pIDs = []
     chat_messages = []
     chat_times = []
     chat_lengths = []
-    
     for file_path in ftchat_file_path_list:
         with open(file_path) as filepath:
             ftchat = BeautifulSoup(filepath,"xml")
@@ -512,13 +535,21 @@ report_file_path = '/Users/JeffHalley/Adobe Connect Project/2-12_dolphin_class.c
 
 #start running for 4-15 5:15 PM class
 recording_folder_path = '/Users/JeffHalley/Adobe Connect Project/adobe connect recording files/'
-report_file_path = '/Users/JeffHalley/Adobe Connect Project/5-15_april-15_class.csv'
+report_file_path = '/Users/JeffHalley/Adobe Connect Project/MODIFIED5-15_april-15_class.csv'
+
+##start running for Environmental Science Week 18 Meeting 2
+recording_folder_path = '/Users/JeffHalley/Adobe Connect Project/Week18mtg2/'
+report_file_path = '/Users/JeffHalley/Adobe Connect Project/es_week_18_2.csv'
+
+###start running for Environmental Science Week 31 Meeting 2
+recording_folder_path = '/Users/JeffHalley/Adobe Connect Project/Week31mtg2/'
+report_file_path = '/Users/JeffHalley/Adobe Connect Project/es_week_31_2.csv'
 
 ###
 
-index_stream_xml_path = get_index_stream_xml_path(recording_folder_path)
-student_ids,student_pIDs = get_student_ids_and_pIDs(index_stream_xml_path)
-ftstage_file_path = get_ftstage_file_path(recording_folder_path)
+index_stream = get_index_stream(recording_folder_path)
+student_ids,student_pIDs = get_student_ids_and_pIDs(index_stream)
+ftstage = get_ftstage(recording_folder_path)
 participant_names = get_participant_names(student_ids)
 
 (
@@ -526,21 +557,21 @@ participant_names = get_participant_names(student_ids)
  student_minutes_with_camera_paused,
  student_fraction_of_class_on_camera,
  student_fraction_of_instructor_time_on_camera
- )= get_camera_contributions(index_stream_xml_path,ftstage_file_path,student_ids)
+ )= get_camera_contributions(index_stream,ftstage,student_ids)
 
 
 (
  student_minutes_on_microphone, 
  student_fraction_of_class_on_microphone,
  student_fraction_of_instructor_mic
- ) = get_microphone_contributions(index_stream_xml_path,student_ids)
+ ) = get_microphone_contributions(index_stream,student_ids)
 
 (
  student_chat_times,
  student_chat_lengths,
  student_message_count,
  student_fraction_of_chats
- )= get_chat_contributions(index_stream_xml_path,student_pIDs)
+ )= get_chat_contributions(index_stream,student_pIDs)
 
 student_participation_grades = get_participation_grades(student_time_on_camera, 
                                                        student_fraction_of_instructor_time_on_camera, 
@@ -548,32 +579,34 @@ student_participation_grades = get_participation_grades(student_time_on_camera,
                                                        student_fraction_of_instructor_mic, 
                                                        student_message_count)
 
-dicts = [
-        participant_names,
-        student_participation_grades,
-        student_time_on_camera,
-        student_minutes_with_camera_paused,
-        student_fraction_of_class_on_camera,
-        student_fraction_of_instructor_time_on_camera,
-        student_minutes_on_microphone, 
-        student_fraction_of_class_on_microphone,
-        student_fraction_of_instructor_mic,
-        student_message_count,
-        student_fraction_of_chats
-    ]    
+class_data = [
+                participant_names,
+                student_participation_grades,
+                student_time_on_camera,
+                student_minutes_with_camera_paused,
+                student_fraction_of_class_on_camera,
+                student_fraction_of_instructor_time_on_camera,
+                student_minutes_on_microphone, 
+                student_fraction_of_class_on_microphone,
+                student_fraction_of_instructor_mic,
+                student_message_count,
+                student_fraction_of_chats
+                ]    
 
 
 results = defaultdict(list)
 for k in participant_names.keys():
-    for item in dicts: 
+    for item in class_data: 
         results[k].append(item.get(k,0))
     
 
 # convert results to list for sorting
-results = list(dict.values(results))
+results = list(results.values())
+
 #sort orders by student name
 results.sort()
 results.sort(key=lambda n: n[0].split()[1])
+
 #make headers for results.csv file
 headers = [
     "Participant",
@@ -588,6 +621,7 @@ headers = [
     "Chat Messages Sent",
     "Fraction of Messages Sent"
     ]
+
 #add headers to results list
 results.insert(0,headers)   
 results
@@ -607,7 +641,7 @@ for result in range(len(results[0])):
        right=participation_data, color="navy")
     plots.append(p)
 
-g = gridplot([plots[1:2],plots[2:6],plots[6:9],plots[9:13]])
+g = gridplot([plots[1:2],plots[2:4],plots[6:7],plots[9:10]])
 
 show(g)
 
